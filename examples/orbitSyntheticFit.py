3bf062eaca5e3d9a5ac689db080791a366bbd5f7
Abby Zhou <76419961+abbyyz@users.noreply.github.com>
2025-07-23 12:23:22 -0700

    edits

--- examples/orbitSyntheticFit.py
+++ examples/orbitSyntheticFit.py
@@ -3,7 +3,7 @@ from lcls_tools.common.data.bmad_modeling import bmad_modeling as mod
 from lcls_tools.common.data.bmad_modeling.outputs import (
     bmad_modeling_outputs as outfn,
 )
-OPTIONS = -"slice BEGINNING:ENDL3B -noplot "
+OPTIONS = "-slice BEGINNING:ENDL3B -noplot "
 INIT = f"-init $LCLS_LATTICE/bmad/models/sc_sxr/tao.init {OPTIONS}"
 
 section = "BEGL3B:ENDL3B"
