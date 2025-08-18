25cbfa43d2a497275cca8ef6f87fb24c016db8af
abbyyz <ajiaqi.zhou@gmail.com>
2025-08-18 03:17:41 -0700

    added fitting tools files

--- examples/orbit_fit_tools.py
+++ examples/orbit_fit_tools.py
@@ -1,12 +1,14 @@
-#gets orbit
+import numpy as np
+import matplotlib.pyplot as plt
+
 def get_orbit(tao):
     orbit_data = {}
     meas, model, design, useit = {},{},{},{}
     for plane in ['x','y']:
         val = tao.data_d_array('orbit', plane)
-        meas[plane] = [item['meas_value'] for item in val]
-        model[plane] = [item['model_value'] for item in val]
-        design[plane] = [item['design_value'] for item in val]
+        meas[plane] = [1000* item['meas_value'] for item in val]
+        model[plane] = [1000*item['model_value'] for item in val]
+        design[plane] = [1000*item['design_value'] for item in val]
         useit[plane] = [item['useit_opt'] for item in val]
     orbit_data['meas'] = meas
     orbit_data['model'] = model
@@ -17,20 +19,80 @@ def get_orbit(tao):
     orbit_data['useit'] = useit
     return orbit_data
 
-#plots the orbits, given two orbit data dictionaries
-#o1 is the orbit data from the model, o2 is the orbit data from the
-#data, type1 and type2 are the keys in the orbit data dictionaries
-#that contain the x and y values to plot
-#e.g. type1 = 'meas', type2 = 'model'
-import matplotlib.pyplot as plt
-import numpy as np
-def plot_orbits(o1, type1, o2,type2):
+def plot_orbits(o1, type1, o2,type2, title_v='',figN = 1):
     indx = np.where(o1['useit']['x'])[0].astype(int)
-    fig, ax = plt.subplots(1, 2, figsize=(12, 5))
-    ax[0].stem(o1['s'], o1[type1]['x'])
-    ax[0].plot(o2['s'], o2[type1]['x'])
-    ax[0].plot(o2['s'][indx[0]:indx[-1]], o2[type1]['x'][indx[0]:indx[-1]], color = 'cyan')
-    ax[1].stem(o1['s'], o1[type1]['y'])
-    ax[1].plot(o2['s'], o2[type1]['y'])
-    ax[1].plot(o2['s'][indx[0]:indx[-1]], o2[type1]['y'][indx[0]:indx[-1]], color = 'cyan')
+    plt.figure(figN)
+    ax1 = plt.subplot(211)
+    ax2 = plt.subplot(212)
+    ax1.stem(o1['s'], o1[type1]['x'])
+    ax1.plot(o2['s'], o2[type2]['x'])
+    ax1.plot(o2['s'][indx[0]:indx[-1]],
+             o2[type2]['x'][indx[0]:indx[-1]], color = 'cyan')
+    ax1.set_title(title_v)
+    ax1.set_ylabel('X (mm)')
+    ax2.stem(o1['s'], o1[type1]['y'])
+    ax2.plot(o2['s'], o2[type2]['y'])
+    ax2.plot(o2['s'][indx[0]:indx[-1]],
+             o2[type2]['y'][indx[0]:indx[-1]], color = 'cyan')
+    ax2.set_ylabel('Y (mm)')
+    ax2.set_xlabel('Z (m)')
     plt.show(block=False)
+
+def plot_orbit_residuals(tao):
+    for u in range(1,37):
+        plt.figure
+        ax1 = plt.subplot(211)
+        ax2 = plt.subplot(212)
+        tao.cmd(f'set default universe = {u}')
+        o = get_orbit(tao)
+        res_x, res_y = [], []
+        for indx in range(0, len(o['meas']['x'])):
+            res_x.append(o['meas']['x'][indx] -
+                         o['model']['x'][indx])
+            res_y.append(o['meas']['y'][indx] -
+                         o['model']['y'][indx])
+        ax1.plot(o['s'], res_x)
+        ax2.plot(o['s'], res_y)
+    ax1.set_title('Residuals')
+    ax1.set_ylabel(' X (mm)')
+    ax1.grid(True)
+    ax2.set_ylabel(' Y (mm)')
+    ax2.set_xlabel(' z (m)')
+    ax2.grid(True)
+    plt.show(block=False)
+    
+
+    
+    
+def show_quad_difference(tao):
+    print('B1_GRADIENT:')
+    print(f'QUAD Model Desing %')
+    val = tao.var_v_array('quad')
+    for indx, v in enumerate(val):
+        name = v['var_attrib_name']
+        model = v['model_value']
+        design = v['design_value']
+        percent = 100 * (model-design)/design if design != 0 else 0
+        print(f'{name:12} {model:8.2f} {design:8.2f} {percent:8.1f}')
+
+
+    
+def allXto(xlim):
+    fig_numbers = plt.get_fignums()
+    for fig_num in fig_numbers:
+        fig = plt.figure(fig_num)
+        axes = fig.get_axes()
+        for ax in axes:
+            ax.set_xlim(xlim)
+        plt.draw()
+    
+def allYto(ylim):
+    fig_numbers = plt.get_fignums()
+    for fig_num in fig_numbers:
+        fig = plt.figure(fig_num)
+        axes = fig.get_axes()
+        for ax in axes:
+            ax.set_ylim(ylim)
+        plt.draw()
+    
+    
